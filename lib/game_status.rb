0a6b0cb6d23fcd51a1b9e38c639c17c62976a74b
Tyler Hunt <tyhunt04@gmail.com>
2019-02-11 18:07:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ def won?(board)
 
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return win_combination
-    else 
+    else
       return false
     end
   end
