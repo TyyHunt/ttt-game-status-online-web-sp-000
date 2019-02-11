ef4512b1f3fb77203665a96baf7855f67a3838b7
Tyler Hunt <tyhunt04@gmail.com>
2019-02-11 17:48:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,5 +2,5 @@
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # Define your WIN_COMBINATIONS constant
